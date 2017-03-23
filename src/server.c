bf34f751947e6fd83c2efe45d07a5bcaed241fd2
Daru13 <daru14@gmail.com>
2017-03-23 18:25:32 +0100

    Added some more socket-handling functions, as well as structs to gather server's info

--- src/server.c
+++ src/server.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include "toolbox.h"
 #include "server.h"
 
 // -----------------------------------------------------------------------------
-// SOCKET AND CONNECTION
+// BASIC WEB SOCKET FUNCTIONS
 // -----------------------------------------------------------------------------
 
 int createWebSocket ()
@@ -15,20 +17,57 @@ int createWebSocket ()
                         SOCK_STREAM, /* TCP  */
                         0);
     if (sockfd < 0)
-    {
-        perror("socket() failed");
-        exit(1);
-    }
+        handleErrorAndExit("socket() failed");
 
     return sockfd;
 }
 
+/*
 void connectWebSocket (const int sockfd, const struct sockaddr *address)
 {
-    int return_value = connect(sockfr, address, sizeof address);
+    int return_value = connect(sockfd, address, sizeof address);
     if (return_value < 0)
     {
         perror("connect() failed");
         exit(1);
     }
 }
+*/
+
+struct sockaddr_in getLocalAddress (const int port)
+{
+    struct sockaddr_in address;
+
+    address.sin_family      = AF_INET;
+    address.sin_port        = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+    memset(address.sin_zero, 0, 8);
+
+    return address;
+}
+
+void bindWebSocket (const int sockfd, const struct sockaddr *address)
+{
+    int return_value = bind(sockfd, address, sizeof address);
+    if (return_value < 0)
+        handleErrorAndExit("bind() failed");
+}
+
+void listenWebSocket (const int sockfd, const int queue_max_length)
+{
+    int return_value = listen(sockfd, queue_max_length);
+    if (return_value < 0)
+        handleErrorAndExit("listen() failed");
+}
+
+int acceptWebSocket (const int sockfd, struct sockaddr* address)
+{
+    socklen_t address_length = sizeof address;
+
+    int clientfd = accept(sockfd, address, &address_length);
+    if (clientfd < 0)
+        handleErrorAndExit("listen() failed");
+
+    return clientfd;
+}
+
--- src/server.h
+++ src/server.h
@@ -1,5 +1,27 @@
 #ifndef __H_SERVER__
 #define __H_SERVER__
 
+#include <netinet/in.h>
+
+// Structures used tor epresent a server
+typedef struct ServParameters {
+	int queue_max_length;
+	// ...
+} ServParameters;
+
+
+typedef struct Server {
+	int sockfd;
+	// ...
+} Server;
+
+// -----------------------------------------------------------------------------
+
+int createWebSocket ();
+void connectWebSocket (const int sockfd, const struct sockaddr *address);
+struct sockaddr_in getLocalAddress (const int port);
+void bindWebSocket (const int sockfd, const struct sockaddr *address);
+void listenWebSocket (const int sockfd, const int queue_max_length);
+int acceptWebSocket (const int sockfd, struct sockaddr* address);
 
 #endif
